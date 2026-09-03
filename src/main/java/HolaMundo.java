2378c97d191ab742e14116de7f086eb03516bf86
phuenchuman01-lang <p.huenchuman01@ufromail.cl>
2026-09-03 00:40:13 -0400

    Saludo de Patricio Huenchuman

--- src/main/java/HolaMundo.java
+++ src/main/java/HolaMundo.java
@@ -2,4 +2,7 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!!!!");
     }
+    public static void SaludoPatricio(String[] args) {
+        System.out.println("Hola. soy Patricio Huenchuman Calful");
+    }
 }
